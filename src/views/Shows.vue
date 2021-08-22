38d1a79a3808ab059663faf38721f4fa0b9a6c30
tuchella <lars-tuchel@outlook.com>
2021-08-22 16:31:16 +0200

    'Remove' show search field for now

--- src/views/Shows.vue
+++ src/views/Shows.vue
@@ -2,9 +2,11 @@
   <v-container>
     <v-row>
       <v-col cols="12" lg="8" style="display:flex; justify-content: flex-end; align-items: center; flex-wrap: wrap;">
+        <!--
         <v-autocomplete label="show" append-outer-icon="mdi-magnify" value=" " placeholder=" " :persistent-placeholder="true" :items="residencies"
           v-model="filter.residency" @change="onFilter" clearable @click:clear=onFilter>
         </v-autocomplete>
+        -->
         <v-text-field class="mx-2" label="from" type="date" placeholder="dd/mm/yyyy" :persistent-placeholder="true"
           v-model="filter.from" @change="onFilter" clearable @click:clear=onFilter
         ></v-text-field>
@@ -96,13 +98,14 @@
             </v-card-actions>
           </v-card>
         </v-dialog>
-
+        <!--
         <v-tooltip bottom>
           <template v-slot:activator="{ on, attrs }">
             <v-btn icon :to="{ name: 'Upload', params: { id: item.id }}" v-bind="attrs" v-on="on"><v-icon>mdi-calendar-cursor</v-icon></v-btn>
           </template>
           <span>Add to program</span>
         </v-tooltip>
+        -->
         <v-tooltip bottom>
           <template v-slot:activator="{ on, attrs }">
             <v-btn icon :to="{ name: 'Upload', params: { id: item.id }}" v-bind="attrs" v-on="on"><v-icon>mdi-cloud-upload-outline</v-icon></v-btn>
